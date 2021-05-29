1d46814df25d8613dddffe2de943e9dce0925d0d
sathianpalanisamy <38859861+sathian123@users.noreply.github.com>
2021-05-29 11:38:09 +0530

    241. Step 10 : More JPA Repository : findById and findAll

--- jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learning.jpa.jpain10steps;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +24,13 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		User user = new User("sathian", "admin");
 		userRepository.save(user);
-
 		logger.info("User is Created : " + user);
+
+		Optional<User> userIdwithOne = userRepository.findById(1l);
+		logger.info("User ID with one is Created : " + userIdwithOne);
+
+		List<User> allUsers = userRepository.findAll();
+		logger.info("All Users : " + allUsers);
 
 	}
 
