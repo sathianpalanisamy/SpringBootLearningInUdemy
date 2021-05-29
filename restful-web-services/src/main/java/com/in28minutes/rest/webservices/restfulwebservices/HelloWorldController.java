6c74dec170850aedcac04393a97bb978473b6b6c
sathianpalanisamy <38859861+sathian123@users.noreply.github.com>
2021-05-29 17:13:26 +0530

    16. Step 04 - Enhancing the Hello World Service to return a Bean

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -12,5 +12,10 @@ public class HelloWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping("/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World With Bean");
+	}
 
 }
