2dcc846419dc3c1ff73ebcd10b13f84945644501
sathianpalanisamy <38859861+sathian123@users.noreply.github.com>
2021-05-30 12:32:41 +0530

    19. Step 07 - Creating User Bean and User Service

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,47 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.Date;
+
+public class User {
+
+	private Integer id;
+	private String name;
+	private Date dateOfBirth;
+
+	public User(Integer id, String name, Date dateOfBirth) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + "]";
+	}
+
+}
---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDAOService.java
@@ -0,0 +1,47 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDAOService {
+
+	private static List<User> los = new ArrayList<User>();
+
+	private static int userCount = 3;
+
+	static {
+		los.add(new User(1, "sathian", new Date()));
+		los.add(new User(2, "nivetha", new Date()));
+		los.add(new User(3, "roopshree", new Date()));
+
+	}
+
+	public List<User> findAll() {
+		return los;
+	}
+
+	public User save(User user) {
+		if (user.getId() == null) {
+			user.setId(++userCount);
+
+		}
+		los.add(user);
+		return user;
+	}
+
+	public User findById(Integer id) {
+		for (User user : los) {
+			if (user.getId() == id) {
+				return user;
+
+			}
+
+		}
+		return null;
+	}
+
+}
