404051cbf87bc93dc8b604ae3c3af53a8a480076
sathianpalanisamy <38859861+sathian123@users.noreply.github.com>
2021-06-26 10:49:07 +0530

    133. Step 11 - Create a simple hard coded currency exchange service - V2

---
+++ currency-exchange/src/main/java/com/in28minutes/microservices/currencyexchange/CurrencyExchange.java
@@ -0,0 +1,52 @@
+package com.in28minutes.microservices.currencyexchange;
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal convertionMultipleReates;
+
+	public CurrencyExchange(Long id, String from, String to, BigDecimal convertionMultipleReates) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.convertionMultipleReates = convertionMultipleReates;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConvertionMultipleReates() {
+		return convertionMultipleReates;
+	}
+
+	public void setConvertionMultipleReates(BigDecimal convertionMultipleReates) {
+		this.convertionMultipleReates = convertionMultipleReates;
+	}
+
+}
---
+++ currency-exchange/src/main/java/com/in28minutes/microservices/currencyexchange/CurrencyExchangeController.java
@@ -0,0 +1,18 @@
+package com.in28minutes.microservices.currencyexchange;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		return new CurrencyExchange(100l, from, to, BigDecimal.valueOf(50));
+
+	}
+
+}
