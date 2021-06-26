011ca1073061d9b5aae3673f8dcc0f31dc1d08bf
sathianpalanisamy <38859861+sathian123@users.noreply.github.com>
2021-06-26 15:47:49 +0530

    140. Step 16 - Creating a service for currency conversion - V2

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,86 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+
+	private long id;
+	private String from;
+	private String to;
+	private BigDecimal convertionMultipleRates;
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmount;
+	private String enviroment;
+
+	public CurrencyConversion() {
+	}
+
+	public CurrencyConversion(long id, String from, String to, BigDecimal multipleConvertion, BigDecimal quantity,
+			BigDecimal totalCalculatedAmount, String enviroment) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.convertionMultipleRates = multipleConvertion;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.enviroment = enviroment;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public BigDecimal getConvertionMultipleRates() {
+		return convertionMultipleRates;
+	}
+
+	public void setConvertionMultipleRates(BigDecimal convertionMultipleRates) {
+		this.convertionMultipleRates = convertionMultipleRates;
+	}
+
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+
+	public String getEnviroment() {
+		return enviroment;
+	}
+
+	public void setEnviroment(String enviroment) {
+		this.enviroment = enviroment;
+	}
+
+}
